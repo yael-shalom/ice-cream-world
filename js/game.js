36fc48aab4f376170a0b48a2ef1e9da9e8e968bf
michalsegal11 <michal.segal11@gmail.com>
2024-03-27 22:11:39 +0300

    xxx

--- js/game.js
+++ js/game.js
@@ -1,24 +1,22 @@
-const cones = ['cone2','cone3','cone5','cone7','cone8','cone9','cone10','cone11'];
-const boys = ['boy1','happyboy1'];
-const balls = ['brown','lightblue','pink','purple','yellow'];
-const decorations = ['almonds','bege-nuts','blueberries','brown-nuts','candies','chocolate','colorful1','colorful2',
-'colorful3','concours','cream1','cream2','cream3','cream4','cream5','cream6','green-almonds','leaf','lemon','macron',
-'orange','strawberries','strawberry','sweet','yellow-fruit'];
+const cones = ['cone2', 'cone3', 'cone5', 'cone7', 'cone8', 'cone9', 'cone10', 'cone11'];
+const boys = ['boy1', 'happyboy1'];
+const balls = ['brown', 'lightblue', 'pink', 'purple', 'yellow'];
+const decorations = ['almonds', 'bege-nuts', 'blueberries', 'brown-nuts', 'candies', 'chocolate', 'colorful1', 'colorful2',
+    'colorful3', 'concours', 'cream1', 'cream2', 'cream3', 'cream4', 'cream5', 'cream6', 'green-almonds', 'leaf', 'lemon', 'macron',
+    'orange', 'strawberries', 'strawberry', 'sweet', 'yellow-fruit'];
 var randomIceCream = {};
 
 randIceCream();
 
-function randIceCream()
-{
-    randomIceCream['cone'] = cones[Math.floor(Math.random()*cones.length)];
-    randomIceCream['ball'] = balls[Math.floor(Math.random()*balls.length)];
-    randomIceCream['decoration'] = decorations[Math.floor(Math.random()*decorations.length)];
+function randIceCream() {
+    randomIceCream['cone'] = cones[Math.floor(Math.random() * cones.length)];
+    randomIceCream['ball'] = balls[Math.floor(Math.random() * balls.length)];
+    randomIceCream['decoration'] = decorations[Math.floor(Math.random() * decorations.length)];
 }
 
 showIceCream();
 
-function showIceCream()
-{
+function showIceCream() {
     const container = document.querySelector('.icecream-container');
     let c = document.createElement('img');
     c.src = `../assets/images/cones/${randomIceCream['cone']}.png`;
@@ -37,4 +35,4 @@ function showIceCream()
 
 
 
-moveBy 
+function moveBy() { }
