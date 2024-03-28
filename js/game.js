5630d84ad64c445095aea71fcef85f77287f8686
michalsegal11 <michal.segal11@gmail.com>
2024-03-28 11:37:14 +0200

    m

--- js/game.js
+++ js/game.js
@@ -37,9 +37,20 @@ function changeIceCream() {
   }
   
 
-mn
+// mn
+//   //rand iceCream:
+// function randIceCream() {
+//     randomIceCream['cone'] = cones[rand(cones.length)];
+//     randomIceCream['ball'][0] = balls[rand(balls.length)];
+//     randomIceCream['ball'][1] = balls[rand(balls.length)];
+//     randomIceCream['ball'][2] = balls[rand(balls.length)];
+//     randomIceCream['decoration'] = decorations[rand(decorations.length)];
+
+// }
+
+
   //rand iceCream:
-function randIceCream() {
+  function randIceCream() {
     randomIceCream['cone'] = cones[rand(cones.length)];
     randomIceCream['ball'][0] = balls[rand(balls.length)];
     randomIceCream['ball'][1] = balls[rand(balls.length)];
