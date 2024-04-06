d1a07cf9fd07d6416fe78c760a7153aa7369ca77
michalsegal11 <michal.segal11@gmail.com>
2024-04-07 02:45:30 +0300

    welcomeFinal

--- js/gameSite.js
+++ js/gameSite.js
@@ -1,4 +1,12 @@
+
+init();
+function init()
+{
+    document.getElementById("one").addEventListener('click',iceCreamGame);
+    
+}
 function iceCreamGame()
 {
+    
     window.open('../pages/game.html','_self');
 }
